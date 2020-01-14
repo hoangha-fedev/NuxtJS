d38d22f0adcf5efb15c1aea6343c6342e6aa75ae
hoangha-fedev <hoangha.fedev@gmail.com>
2020-01-14 15:56:16 +0700

    News

---
+++ News/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <v-container>
+            <h2>About</h2>
+
+            <v-layout>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias sequi modi saepe illo? Incidunt neque corrupti nam dignissimos corporis quidem quisquam fuga maiores praesentium consectetur, beatae delectus assumenda modi libero!
+            </v-layout>
+        </v-container>
+    </div>
+</template>
---
+++ News/pages/news.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-container>
+      <v-flex xs12 pb-5>
+        <h2>News</h2>
+      </v-flex>
+
+      <v-layout class="news">
+        <v-flex class="col-3" v-for="(article, index) in articles" :key="index">
+          <!-- News -->
+          <v-card class="mx-auto">
+            <img :src="article.urlToImage" width="100%" height="200px" />
+
+            <v-card-title>
+              {{ article.title }}
+            </v-card-title>
+
+            <v-card-subtitle>
+              1,000 miles of wonder
+            </v-card-subtitle>
+            <v-card-text>
+              {{ article.content }}
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn :href="article.url" target="_blank">Open</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: false
+  }),
+  async asyncData({ app }) {
+    const { articles } = await app.$axios.$get(
+      "https://newsapi.org/v2/everything?q=bitcoin&from=2019-12-13&sortBy=publishedAt&apiKey=9b96c965994f43a6adc455e8ab0ff883"
+    );
+    return { articles };
+  }
+};
+</script>
+
+<style lang="scss">
+.news {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
