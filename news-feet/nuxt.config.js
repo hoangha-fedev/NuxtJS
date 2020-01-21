f6e78d8547a748b605cb1c6325b1ee6f1cc041aa
hoangha-fedev <hoangha.fedev@gmail.com>
2020-01-21 17:50:32 +0700

    news-feed

---
+++ news-feet/nuxt.config.js
@@ -0,0 +1,77 @@
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#9ccc65', height: '10px' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    { src: 'vue-material/dist/vue-material.min.css', lang:'css' },
+    { src: '~/assets/themes.scss', lang: 'scss' }
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~/plugins/vue-material'},
+    { src: '~/plugins/axios'},
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+    credentials: true,
+    proxy: true
+  },
+  proxy: {
+    "/api" : {
+      target: "https://newsapi.org/v2/",
+      pathRewrite: { "^/api/": ""}
+    }
+  },
+  env: {
+    NEW_API_KEY: "9b96c965994f43a6adc455e8ab0ff883"
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
